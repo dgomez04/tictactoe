b3e9dd739895aa6e2d578d746cf27a05d2ecd269
dgomez04 <dagomezmoreno@outlook.com>
2023-05-30 15:37:05 -0600

    added initial files and started structure

---
+++ script.js
@@ -0,0 +1,19 @@
+
+// gameBoard controller -> controls board and its attributes
+
+const gameBoard = (() => {
+
+    const gameBoard = new Array(9)
+
+    // getTile
+
+    // setTile
+    
+    //clear
+})();
+
+// displayController -> controls display between front and back
+
+// gameController -> controls gameplay, creates players
+
+// Player -> sets symbol to be either "x" or "y"

ebf9ce167d67fb47216429edd43b27997a6be21c
dgomez04 <dagomezmoreno@outlook.com>
2023-05-31 13:24:10 -0600

    added npm build script and finished checkWin() function logic

--- script.js
+++ script.js
@@ -3,50 +3,63 @@
 
 const gameBoard = (() => {
 
-    const Board = new Array(9)
+    const Board = ["", "", "", "", "", "", "", "", ""]
 
-    // getTile
     const getTile = (index) =>  Board[index]; 
 
-    // setTile
     const setTile = (index, sign) => Board[index] = sign;
     
-    //checkWin -> goes through array to check for winner
+    //TO DO: find a way to write the function cleaner
+    const checkWin = () => {
+        const winCombo = [
+            [0,1,2], [3,4,5], 
+            [6,7,8], [0,3,6], 
+            [1,4,7], [2,5,8],
+            [0,4,8], [2,4,6]
+        ]
 
-    //clear
-    const clearBoard = () => {
-        for(let i = 0; i < Board.length; i++)
-            Board[i] = undefined;
+        for(let i = 0; i < winCombo.length; i++) {
+            return((gameBoard.getTile(winCombo[i][0]) != "" || gameBoard.getTile(winCombo[i][0]) 
+            === gameBoard.getTile(winCombo[i][1] === gameBoard.getTile(winCombo[i][2]))) ? true : false)
+        }
     };
 
-    return {getTile, setTile, clearBoard};
+    const clearBoard = () => {
+        for(let i = 0; i < Board.length; i++)
+            Board[i] = "";
+    };
+
+    return {getTile, setTile, checkWin, clearBoard};
 })();
 
-// displayController -> associates display with backend
+// displayController -> associates display with logic
 
 const displayController = (() => {
 
     const tileSet = document.querySelectorAll('#gridcontainer > *');
 
-    // checkTile (if getTile != "")
     function checkTile(index) {
         return (gameBoard.getTile(index) != undefined) ? true : false;
     }
 
-    // drawTile -> draw all Tiles
-    const drawTiles = () => {
+    const updateTiles = () => {
         tileSet.forEach((tile) => {
             tile.textContent = gameBoard.getTile(tile.dataset.key);
         });
     }
 
-    return {checkTile, drawTiles}
+    return {checkTile, updateTiles}
 })();
-
-// gameController -> controls gameplay, creates players, finds winner
 
 // Player -> sets symbol to be either "x" or "o"
 
 const Player = ((sign) => {
     return {sign}
 });
+
+// gameController -> controls gameplay, playRound
+const gameController = (() => {
+    const playerOne =  Player('X');
+    const playerTwo =  Player('O');
+})();
+
